f12e481023704c1f0a2f538e44acc2c29dbc2111
unknown <marciatadakuma@gmail.com>
2021-12-17 01:39:38 -0300

    :turtle:

--- src/componentes/Conteudo/Conteudo.jsx
+++ src/componentes/Conteudo/Conteudo.jsx
@@ -10,16 +10,19 @@ const Conteudo = () => {
            
             <div class="child">
 
-               <img className="tortuga" src={ImageTurtle} title="Taruaruga-verde" alt='logo'/>  
+               <img className="tortuga" src={ImageTurtle} title="Tartaruga-verde" alt='logo'/>  
                
-               <p className=" primeiro-paragrafro">São répteis existentes há mais de 180 milhões de anos e teriam evoluído a partir das tartarugas de água doce. A carapaça das tartarugas marinhas é mais achatada, o que as deixam mais leves e hidrodinâmicas. Possuem visão, olfato e audição desenvolvidos e as patas são eficientes nadadeiras debaixo d´agua. Outra importante adaptação foi o surgimento de glândulas de sal, localizadas próximo aos olhos. A maioria das espécies de tartarugas marinhas é migratória e se orientam pelo oceano com a ajuda do campo magnético terrestre.</p>
+               <p className=" primeiro-paragrafro">São répteis existentes há mais de 180 milhões de anos e teriam evoluído a partir das tartarugas de água doce. Você sabia que há somente sete espécies de tartarugas marinhas no mundo? 
+               É isso mesmo! Dessas sete, cinco ocorrem no litoral brasileiro: a tartaruga-cabeçuda, a tartaruga-de-pente, a tartaruga-verde, a tartaruga-oliva e a tartaruga-de-couro..</p>
+
+
                
             </div>
                 <div class="child">
                      
                     <p className=" segundo-paragrafro">A poluição é uma das principais ameaças, pois degradam o ambiente marinho como um todo..<p>  clique aqui <a  href="https://www.youtube.com/watch?v=P8dQGL7jYg4"></a></p> </p>
                      
-                    <img className="tortuga" src={picTorguga} alt='logo'/>  
+                    <img className="tortuga" src={picTorguga} alt='foto de tartaruga marinha'/>  
                    
                      
                </div> 
--- src/pages/Sobre/Sobre.js
+++ src/pages/Sobre/Sobre.js
@@ -5,8 +5,13 @@ import ImageSobre from '../../assets/sobre.png'
 import Logo from '../../assets/logo-branco.png'
 import picTorguga from "../../assets/Tartaruga-planeta.png"
 import Quiz from '../../componentes/Quiz/Quiz'
-
-
+import tverde from "../../assets/t-verd.jpg"
+import taustriana from "../../assets/t-australiana.jpg"
+import tcouro from "../../assets/t-couro.jpg"
+import tolivia from "../../assets/t-oliva.jpg"
+import tcabecuda from "../../assets/t-cabecuda.jpg"
+import tpente from "../../assets/t-pente.jpg"
+import tflatback from "../../assets/t-flatback.jpg"
 
 import '../Sobre/sobre.styles.css'
 
@@ -17,14 +22,18 @@ const Sobre = () => {
     <img className="logoImagem" src={Logo} alt='logo'/>
     <Banner className="imagem" image={ImageSobre} > </Banner>
     <>
-    <div className="container" >
-      <div class="child"><img className="tortuga" src={picTorguga} alt='logo'/>  </div>
-      <div className="child"><img className="tortuga" src={picTorguga} alt='logo'/></div>
-      <div className="child"><img className="tortuga" src={picTorguga} alt='logo'/></div>
-      <div className="child"><img className="tortuga" src={picTorguga} alt='logo'/></div>
-      <div className="child"><img className="tortuga" src={picTorguga} alt='logo'/></div>
-      <div className="child"><img className="tortuga" src={picTorguga} alt='logo'/></div>
-      <div className="child"><img className="tortuga" src={picTorguga} alt='logo'/></div>
+    <h1>Tipos de tartarugas</h1>
+    <div className="caixao" >
+      
+      <div className="caixa-foto"><img className="tortu" src={tverde} alt='tartaruga verde'/>  </div>
+      
+      <div className="caixa-foto"><img className="tortu" src={taustriana} alt='logo'/></div>
+      <div className="caixa-foto"><img className="tortu" src={tcouro} alt='logo'/></div>
+      <div className="caixa-foto"><img className="tortu" src={tolivia} alt='logo'/></div>
+      
+      <div className="caixa-foto"><img className="tortu" src={tcabecuda } alt='logo'/></div>
+      <div className="caixa-foto"><img className="tortu" src={tpente} alt='logo'/></div>
+      <div className="caixa-foto"><img className="tortu" src={tflatback} alt='logo'/></div>
       
 
 
